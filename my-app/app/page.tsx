4a67d12e84025db53cfe87da5d951fe0d475985f
Keanu de Hart <2686691@students.wits.ac.za>
2026-08-04 16:28:48 +0200

    feat: update note section header and structure for clarity, added details. So now its a dropdown

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,14 +3,17 @@ import { createNote } from "./actions";
 export default function Home() {
   return (
     <main>
-      <h1>New note</h1>
+      <h1>Notes</h1>
 
+      <details>
+        <summary>New Note</summary>
       <form action={createNote}>
 
         <label htmlFor="body">Note</label>
         <textarea id="body" name="body" rows={8} />
         <button type="submit">save</button>
       </form>
+      </details>
     </main>
   );
 
